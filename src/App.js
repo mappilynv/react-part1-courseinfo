61fd6f6ae88d0ddbdc33a39dec2d7962a2865d9f
Queen Chungus <maprg.nv@gmail.com>
2022-04-14 17:34:33 -0400

    uploaded files first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  {/* Header takes care of rendering the name of the course */}
+  const Header = (props) => {
+    return (
+      <div>
+        <h1>{props.course}</h1> 
+      </div>
+    )
+  }
+  {/* Refactor the Content component so that it does not render any names of parts or their number of exercises by itself */}
+  const Part = (props) => {
+    return(
+      <p>{props.part} {props.total}</p>
+    )
+  };
 
-function App() {
+  {/* Content renders the parts and their number of exercises */}
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} total={exercises1}/>
+        <Part part={part2} total={exercises2}/>
+        <Part part={part3} total={exercises3}/>
+      </div>
+    )
+  }
+
+  {/* Total renders the total number of exercises. */}
+  const Total = (props) => {
+    return (
+      <div>
+        <p>Number of exercises: {exercises1 + exercises2 + exercises3}</p>    
+      </div>
+    )
+  }
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
     </div>
-  );
+  )
 }
 
 export default App;
